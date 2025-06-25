d6419b32b19e34efd0ce3e477c2a387912edf181
krishikajalali <144584253+krishikajalali@users.noreply.github.com>
2025-06-25 08:37:00 +0000

    var,datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// code readability should be high
+
+//ECMA(tc39)- standards for javascript irrespective of type of browser
+//mdm is also another documentation of javascript 
+
+
+
+let name="kj"
+let age=18
+let booli= false
+
+//PREMITIVE
+//number= 2^53
+//bigint
+//string "" ''
+//boolean true/false
+//null - standalone value (let state=null) use case: no response from some server= null
+//undefined (variable declared but no value assigned) eg let state;
+//symbol (for uniqueness) used in react
+
+//OBJECT
+
+console.log(typeof "hitesh") //string(a datatype)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+// we will discuss reason later
