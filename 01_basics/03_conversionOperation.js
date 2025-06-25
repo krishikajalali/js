5fc5ad1d7bfad4cc5f51d65037c814a7884ea711
krishikajalali <144584253+krishikajalali@users.noreply.github.com>
2025-06-25 14:24:13 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,60 +1,94 @@
-//needed when we are unsure about the datatype of the variables' value we are receiveing
+// //needed when we are unsure about the datatype of the variables' value we are receiveing
 
-let score="33"
+// let score="33"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
 
-score="33abs"
+// score="33abs"
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
-console.log(valueInNumber)// NaN- not a number
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber)// NaN- not a number
 
-score = null
+// score = null
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
-console.log(valueInNumber)// 0
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber)// 0
 
-score = undefined
+// score = undefined
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
-console.log(valueInNumber)// NaN
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber)// NaN
 
-score = true
+// score = true
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
-console.log(valueInNumber)// 1 , 0 for false
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber)// 1 , 0 for false
 
-score ="krishika"
+// score ="krishika"
 
-valueInNumber = Number(score);
-console.log(typeof valueInNumber);// number
-console.log(valueInNumber)// NaN
-
-
-
-let isLoggedIn= 1
-let booleanisLoggedIn= Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
-
-/*
-1 -> true
-0 -> false
-"" -> false
-"anything" -> true
-*/
-
-let somenumber= 33
-let stringnumber= String(somenumber)
-
-console.log(typeof stringnumber)
-console.log(stringnumber)
+// valueInNumber = Number(score);
+// console.log(typeof valueInNumber);// number
+// console.log(valueInNumber)// NaN
 
 
+
+// let isLoggedIn= 1
+// let booleanisLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// /*
+// 1 -> true
+// 0 -> false
+// "" -> false
+// "anything" -> true
+// */
+
+// let somenumber= 33
+// let stringnumber= String(somenumber)
+
+// console.log(typeof stringnumber)
+// console.log(stringnumber)
+
+
+// ************************ OPERATIONS  ************************
+
+let value= 3
+let negvalue= -value
+
+//console.log(negvalue) -> -3
+
+//+,-,*,**,/,%
+
+let str1="krishika"
+let str2=" jalali"
+
+//console.log(str1+str2)
+
+// console.log("1"+2) //12
+// console.log(1+"2") //12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+
+//guide: documentation
+
+// not used
+// console.log(true) // true
+// console.log(+true) // 1
+// console.log(+"") //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 =2 + 2
+
+let gameCounter = 100
+gameCounter++; //or ++gameCounter
+console.log(gameCounter); //101
+// prefix and postfix incremental 
